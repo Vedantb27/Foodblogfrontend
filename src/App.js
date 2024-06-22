11564067bd4c6feac85cefd4df708d2c2ac69685
Vedantb27 <157893464+Vedantb27@users.noreply.github.com>
2024-06-22 20:25:47 +0530

    Cardscontent route done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './Main';
 import './App.css';
+import {Cardscontent} from './MyComponents/Category/Cardscontent'
 
 
 const router = createBrowserRouter([
   { path: "/", element: <Main /> },
+  { path: "/Cardscontent", element: <Cardscontent /> },
 ]);
 
 function App() {
