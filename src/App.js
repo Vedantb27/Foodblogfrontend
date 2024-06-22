37ebcacaa2603cf2ab2246624e60c8a0448e28ee
Vedantb27 <157893464+Vedantb27@users.noreply.github.com>
2024-06-22 20:46:05 +0530

    Admineditcateogry route Done

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './Main';
 import './App.css';
-import {Cardscontent} from './MyComponents/Category/Cardscontent'
+import {Cardscontent} from './MyComponents/Category/Cardscontent';
+import { Admineditcategory } from './MyComponents/Admin/Admineditcategory';
+import {ProtectedRoute}  from './MyComponents/ProtectedRoute';
+import { AdminProvider  } from './MyComponents/Admin/Admincontext';
 
 
 const router = createBrowserRouter([
   { path: "/", element: <Main /> },
   { path: "/Cardscontent", element: <Cardscontent /> },
+  {
+    path: "/Admineditcategory",
+    element: (
+      <ProtectedRoute>
+        <Admineditcategory />
+      </ProtectedRoute>
+    )
+  },
 ]);
 
 function App() {
   return (
-    <>
+    <AdminProvider>
       <RouterProvider router={router} />
-    </>
+    </AdminProvider>
   );
 }
 
