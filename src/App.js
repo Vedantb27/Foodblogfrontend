90c8b8e9207ea2e92c081653119f6605d578c7e6
Vedantb27 <157893464+Vedantb27@users.noreply.github.com>
2024-06-22 18:08:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('https://foodbloggingappbackend.onrender.com/get-json')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
+    axios.get('https://foodbloggingappbackend.onrender.com/get-json')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
 
   return (
