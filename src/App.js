50f048a613a9a3ed16830a51f72f356ba56aef32
Vedantb27 <157893464+Vedantb27@users.noreply.github.com>
2024-06-22 17:10:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://foodbloggingappbackend.onrender.com/get-json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
-      This is react App
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
     </div>
   );
 }
